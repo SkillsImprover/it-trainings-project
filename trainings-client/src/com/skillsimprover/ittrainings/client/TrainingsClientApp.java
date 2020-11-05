d9d7f50be085db278c60306a0475062850b88e6b
mlazarevich <mlazarevich@skillsimprover.com>
2020-11-05 23:24:58 +0300

    Client Module added.

---
+++ trainings-client/src/com/skillsimprover/ittrainings/client/TrainingsClientApp.java
@@ -0,0 +1,8 @@
+package com.skillsimprover.ittrainings.client;
+
+public class TrainingsClientApp {
+
+    public static void main(String[] args) {
+        System.out.println("Client App start and running...");
+    }
+}
