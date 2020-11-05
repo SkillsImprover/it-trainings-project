3edebce044e39297ba75e5ef38f7d7ee32f228d6
mlazarevich <mlazarevich@skillsimprover.com>
2020-11-05 23:50:09 +0300

    Revert "Student class from Data Module has been imported."
    This reverts commit 5bb3786f

--- trainings-client/src/com/skillsimprover/ittrainings/client/TrainingsClientApp.java
+++ trainings-client/src/com/skillsimprover/ittrainings/client/TrainingsClientApp.java
@@ -1,14 +1,8 @@
 package com.skillsimprover.ittrainings.client;
 
-import com.skillsimprover.ittrainings.data.Student;
-
 public class TrainingsClientApp {
 
     public static void main(String[] args) {
         System.out.println("Client App start and running...");
-
-        Student student = new Student("Ivan");
-        System.out.println(student.getFirstName());
-
     }
 }
