a221e9e933de469b7fba74ec5fef34b51856ff8e
mlazarevich <mlazarevich@skillsimprover.com>
2020-11-05 23:32:40 +0300

    Data Module added.

---
+++ trainings-data/src/com/skillsimprover/ittrainings/data/Student.java
@@ -0,0 +1,14 @@
+package com.skillsimprover.ittrainings.data;
+
+public class Student {
+
+    private String firstName;
+
+    public Student(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+}
