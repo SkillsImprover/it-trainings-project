1e434fca8617ba3c7f0c339dc045e3f804e8f3c0
mlazarevich <priobit.email@gmail.com>
2020-11-05 23:16:58 +0300

    Module for server part added.

---
+++ trainings-server/src/com/skillsimprover/ittrainings/server/TrainingsServerApp.java
@@ -0,0 +1,8 @@
+package com.skillsimprover.ittrainings.server;
+
+public class TrainingsServerApp {
+
+    public static void main(String[] args) {
+        System.out.println("Server App start and running...");
+    }
+}
