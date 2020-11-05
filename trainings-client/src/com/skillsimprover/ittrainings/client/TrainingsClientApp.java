5bb3786f0838fe0927e06bc2b4e89d72e1865f07
mlazarevich <mlazarevich@skillsimprover.com>
2020-11-05 23:43:57 +0300

    Student class from Data Module has been imported.

--- trainings-client/src/com/skillsimprover/ittrainings/client/TrainingsClientApp.java
+++ trainings-client/src/com/skillsimprover/ittrainings/client/TrainingsClientApp.java
@@ -1,8 +1,14 @@
 package com.skillsimprover.ittrainings.client;
 
+import com.skillsimprover.ittrainings.data.Student;
+
 public class TrainingsClientApp {
 
     public static void main(String[] args) {
         System.out.println("Client App start and running...");
+
+        Student student = new Student("Ivan");
+        System.out.println(student.getFirstName());
+
     }
 }
